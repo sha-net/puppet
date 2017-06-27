037505d420ed4d7144ff8ae1093d2964a5d34ad7
Shavit Netzer <shavit.netzer@gmail.com>
2017-06-27 15:45:51 +0300

    Update install-puppet-agent-with-hostname-ubuntu.sh

--- install-puppet-agent-with-hostname-ubuntu.sh
+++ install-puppet-agent-with-hostname-ubuntu.sh
@@ -6,8 +6,9 @@ wget https://apt.puppetlabs.com/puppetlabs-release-pc1-${release}.deb
 dpkg -i puppetlabs-release-pc1-${release}.deb
 apt-get update
 apt-get -y install puppet-agent unzip
-sed -i "s/^127.0.0.1 .*$/127.0.0.1 localhost $hostname/g" /etc/hosts
-echo "server = puppet.aws-prod.XXXX.com" >> /etc/puppetlabs/puppet/puppet.conf
-service puppet-agent restart || service puppet restart
-/opt/puppetlabs/bin/puppet agent -t
+#sed -i "s/^127.0.0.1 .*$/127.0.0.1 localhost $hostname/g" /etc/hosts
+#echo "server = puppet.aws-prod.XXXX.com" >> /etc/puppetlabs/puppet/puppet.conf
+#service puppet-agent restart || service puppet restart
+#/opt/puppetlabs/bin/puppet agent -t
 /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
+/opt/puppetlabs/bin/puppet agent -t
